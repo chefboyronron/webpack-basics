41de8f5fd824590ddbd118fd76f89bd391d069fb
chefboyronron <rgpseminiano@gmail.com>
2021-03-18 16:54:53 +0800

    chere: production mode

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('./assets/css/style.scss');
 require('./assets/ts/script.ts');
 let People = require('./assets/js/people.js');
-let $ = require('jquery');
+// Use JQUERY library
+// let $ = require('jquery');
